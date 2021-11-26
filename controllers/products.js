11afdf47caa515c20a1e672d06edc121c7a42278
topzz100 <adenekantope@gmail.com>
2021-11-26 13:03:11 +0100

    some filters in place

--- controllers/products.js
+++ controllers/products.js
@@ -2,8 +2,20 @@ const asyncWrapper = require('../middleware/async-wrapper')
 const Products = require('../db/models/products') 
 
 const getProducts = asyncWrapper(async (req,res) => {
-  const products =await Products.find()
-  res.status(200).json({products, nbHits : products.length})
+  const {name, featured, company} = req.query
+  const queryObject = {}
+
+  if(name){
+    queryObject.name = {$regex : name, $options : 'i'}
+  }
+  if(featured){
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if(company){
+    queryObject.company = company
+  }
+  const products =await Products.find(queryObject)
+  res.status(200).json({nbHits : products.length, products})
 })
 const getProductsStatic = asyncWrapper(async (req,res) => {
  const products = await Products.find()
