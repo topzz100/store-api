9667f8a60be427d08f2a43ba4c60d4c25c6ea2e3
topzz100 <adenekantope@gmail.com>
2021-11-27 14:34:08 +0100

    pagination, skip, limit

--- controllers/products.js
+++ controllers/products.js
@@ -30,7 +30,7 @@ const getProducts = asyncWrapper(async (req,res) => {
 
   const page = Number(req.query.page) || 1
   const limit = Number(req.query.limit) || 10
-  const skip = (page-1)*limit
+  const skip = (page-1) * limit
   const reault = result.skip(skip).limit(limit)
 
   const products = await result
