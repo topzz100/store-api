d4e386d7b0aca01f85d41bf0598c050c05917b73
topzz100 <adenekantope@gmail.com>
2021-11-27 14:31:53 +0100

    .

--- controllers/products.js
+++ controllers/products.js
@@ -28,6 +28,11 @@ const getProducts = asyncWrapper(async (req,res) => {
    result = result.select(fieldsList)  
   }
 
+  const page = Number(req.query.page) || 1
+  const limit = Number(req.query.limit) || 10
+  const skip = (page-1)*limit
+  const reault = result.skip(skip).limit(limit)
+
   const products = await result
   res.status(200).json({nbHits : products.length, products})
 })
